adf06e5f99fe7605b35e1e8166256f218d0eb830
Vedant <vm.WorkAddress@gmail.com>
2023-05-22 00:21:52 -0500

    commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get("/image", (req,res)=>{
 
 // Anything that doesn't match the above, send back the index.html file
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname + '/client/public/index.html'))
+   res.sendFile(path.join(__dirname + '/client/build/index.html'))
  })
 
 // Choose the port and start the server
